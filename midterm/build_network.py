517e49a412c1f535b5db0b8a37ed3806f30b1578
Ashley Doane <ashley.doane@gmail.com>
2015-04-08 12:09:38 -0400

    revisions

--- midterm/build_network.py
+++ midterm/build_network.py
@@ -14,120 +14,51 @@ subprocess.call([cindex], shell=True)
 subprocess.call([c1], shell=True)
 subprocess.call([c2], shell=True)
 
-#subprocess.call(['bwa bwasw dm_coding.fasta dm_seq_R1.fastq > dm_R1.sam'], shell=True)
-#subprocess.call(['bwa bwasw dm_coding.fasta dm_seq_R2.fastq > dm_R2.sam'], shell=True)
-
 R1 = {}
 for line in open("dm_R1.sam"):
     if line[0] == "@":
-        pass
-    else:
-        sl = line.split()
-        QNAME = sl[0]
-        FLAG = sl[1]
-        RNAME = sl[2]
-        POS = sl[3]
-        MAPQ = sl[4]
-        CIGAR = sl[5]
-        SEQ = sl[9]
-        if FLAG != 4 & len(QNAME) >1:
-            R1[QNAME] = {"RNAME":RNAME,
-                         "FLAG":FLAG,
-                         "MAPQ":MAPQ,
-                         "SEQ":SEQ
-            }
+        continue
+    sl = line.strip().split('\t')
+    if sl[1] == '4':
+        continue
+    r_id = sl[0][7:]
+    flag = sl[1]
+    rname = sl[2]
+    pos = sl[3]
+    if r_id not in R1:
+        R1[r_id] = {}
+    R1[r_id][rname] = flag
 
 R2 = {}
 for line in open("dm_R2.sam"):
     if line[0] == "@":
-        pass
-    else:
-        sl = line.split()
-        QNAME = sl[0]
-        FLAG = sl[1]
-        RNAME = sl[2]
-        POS = sl[3]
-        MAPQ = sl[4]
-        CIGAR = sl[5]
-        SEQ = sl[9]
-        if FLAG != 4 & len(QNAME) >2:
-            R2[QNAME] = {"RNAME":RNAME,
-                         "FLAG":FLAG,
-                         "MAPQ":MAPQ,
-                         "SEQ":SEQ
-            }
-R1 = {}
-for line in open("dm_R1.sam"):
-    if line[0] == "@":
-        pass
-    else:
-        sl = line.split()
-        QNAME = sl[0]
-        FLAG = sl[1]
-        RNAME = sl[2]
-        POS = sl[3]
-        MAPQ = sl[4]
-        CIGAR = sl[5]
-        SEQ = sl[9]
-        if FLAG != 4 & len(RNAME) >1:
-            R1[QNAME] = {"RNAME":RNAME,
-                         "FLAG":FLAG,
-                         "MAPQ":MAPQ,
-                         "SEQ":SEQ
-            }
-
-R2 = {}
-for line in open("dm_R2.sam"):
-    if line[0] == "@":
-        pass
-    else:
-        sl = line.split()
-        QNAME = sl[0]
-        FLAG = sl[1]
-        RNAME = sl[2]
-        POS = sl[3]
-        MAPQ = sl[4]
-        CIGAR = sl[5]
-        SEQ = sl[9]
-        if FLAG != 4 & len(RNAME) >1:
-            R2[QNAME] = {"RNAME":RNAME,
-                         "FLAG":FLAG,
-                         "MAPQ":MAPQ,
-                         "SEQ":SEQ
-            }
-
-###for debugging pairs and directions
-pairsD = []
-same_dir = [] #pairs with same direction, for debugging
-for k1, v1 in R1.items():
-    if k1 in R2:
-        #pp = (R1[k1]["RNAME"], R2[k1]["RNAME"])
-        p1, p2 = [R1[k1]["RNAME"],R1[k1]["FLAG"]], [R2[k1]["RNAME"],R2[k1]["FLAG"]]
-        if p1[1] == p2[1]:
-            if (p2, p1) in same_dir:
-                same_dir.append((p2, p1))
-            else:
-                same_dir.append((p1, p2))
-        if p1[1] != p2[1]: #each pair must have 1 fwd and 1 rev comp read
-            if (p2, p1) in pairsD:
-                pairsD.append((p2, p1))
-            else:
-                pairsD.append((p1, p2))
-###
-
+        continue
+    sl = line.strip().split('\t')
+    if sl[1] == '4':
+        continue
+    r_id = sl[0][7:]
+    flag = sl[1]
+    rname = sl[2]
+    pos = sl[3]
+    if r_id not in R2:
+        R2[r_id] = {}
+    R2[r_id][rname] = flag
 
 pairs = []
-for k1, v1 in R1.items():
-    if k1 in R2:
-        #pp = (R1[k1]["RNAME"], R2[k1]["RNAME"])
-        p1, p2 = [R1[k1]["RNAME"],R1[k1]["FLAG"]], [R2[k1]["RNAME"],R2[k1]["FLAG"]]
-        if p1[1] != p2[1]:
-            if (p2[0], p1[0]) in pairs:
-                pairs.append((p2[0], p1[0]))
+for rids in R1:
+    if rids not in R2:
+        continue
+    #pp = (R1[k1]["RNAME"], R2[k1]["RNAME"])
+    for p1 in R1[rids]:
+        for p2 in R2[rids]:
+            if R1[rids][p1] == R2[rids][p2]:continue
+            pp = (p1, p2)
+            if (p2, p1) in pairs:
+                ppr = (p2, p1)
+                pairs.append(ppr)
             else:
-                pairs.append((p1[0], p2[0]))
-
-##need t take care of reverse reduncies here
+                pp = (p1, p2)
+                pairs.append(pp)
 
 from collections import defaultdict
 
@@ -135,15 +66,6 @@ pairsn = defaultdict(int)
 
 for ppi in pairs:
     pairsn[ppi] += 1
-pairsn
-
-
-
-#import collections
-#cnt = collections.Counter(pairs)
-#cnt
-
-
 
 pairsn_s = sorted(pairsn.items(),key=lambda (k,v): v,reverse=True)
 pairsn_s
